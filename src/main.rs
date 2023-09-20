37170d51b0648ceb0666a2a8362b4c30f450e942
boreddad <ronnebaumfinances@gmail.com>
2023-09-20 12:11:38 -0400

    feat: read-parse-print-loop

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
-pub mod token;
-pub mod lexer;
+use ast::Node;
+
 pub mod ast;
+pub mod lexer;
 pub mod parser;
+pub mod token;
 pub mod util;
 
 const PROMP: &'static str = ">> ";
@@ -9,17 +11,29 @@ const PROMP: &'static str = ">> ";
 fn main() -> anyhow::Result<()> {
     loop {
         let line = util::read_line(PROMP)?;
-        let mut l: lexer::Lexer;
-        let mut tok: token::Token;
+        let l: lexer::Lexer;
+        let mut p: parser::Parser;
+        let program: ast::Program;
+        let s: String;
         if line == "exit\n" {
             break;
         }
         l = lexer::Lexer::new(&line);
-        tok = l.next_token();
-        while tok != token::Token::Eof {
-            println!("{:#?}", tok);
-            tok = l.next_token();
+        p = parser::Parser::new(l);
+        program = p.parse();
+        if p.errors_len() != 0 {
+            print_errors(&p);
+            continue;
         }
+        s = program.string();
+        println!("{}", s);
     }
     Ok(())
+}
+
+fn print_errors(p: &parser::Parser) {
+    let errors = p.get_errors();
+    for err in errors.iter() {
+        println!("{}", err);
+    }
 }
